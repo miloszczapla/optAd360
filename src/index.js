ede086fa331362025703b7018dfec1b32039412e
miloszczapla <miloszczapla@gmail.com>
2022-01-29 16:22:40 +0100

    Table added

--- src/index.js
+++ src/index.js
@@ -7,12 +7,89 @@ async function fetchData() {
   return data.results;
 }
 
-async function onStart() {
-  const button = window.querySelector('#button');
-  const chart = window.querySelector('#chart');
-  const table = window.querySelector('#table');
+function changeBacgroundEveryFifthRefreash(element) {
+  //get element and change its background color after getting counter from local storage
+  //if local storage is empty, add counter to local storage
 
-  console.log('fetchChartData', await fetchData());
+  const counter = JSON.parse(sessionStorage.getItem('counter'));
+  if (counter === null) {
+    sessionStorage.setItem('counter', 0);
+  } else {
+    sessionStorage.setItem('counter', counter + 1);
+  }
+
+  if (counter % 5 === 0) {
+    element.style.backgroundColor = '#FF6767';
+  } else {
+    element.style.backgroundColor = 'azure';
+  }
+}
+
+function hydrateTable(data, viewportWidth) {
+  const sortedData = data.sort(
+    (a, b) => new Date(a.dob.date).getTime() - new Date(b.dob.date).getTime(),
+  );
+  const oldestTen = sortedData.slice(0, 10);
+  const table = document.querySelector('#table');
+  const headers = ['Name', 'Picture', 'Age', 'Email', 'Phone', 'Address'];
+
+  if (viewportWidth > 768) {
+    const tr = document.createElement('tr');
+    headers.forEach((header) => {
+      const th = document.createElement('th');
+      th.innerText = header;
+      tr.appendChild(th);
+    });
+    table.appendChild(tr);
+  }
+
+  oldestTen.forEach((person) => {
+    const tr = document.createElement('tr');
+    const tdName = document.createElement('td');
+    const tdAge = document.createElement('td');
+    const tdEmail = document.createElement('td');
+    const tdPhone = document.createElement('td');
+    const tdAddress = document.createElement('td');
+    const tdPicture = document.createElement('td');
+
+    tdName.innerText = person.name.first + ' ' + person.name.last;
+    tdAge.innerText = person.dob.age;
+    tdEmail.innerText = person.email;
+    tdPhone.innerText = person.phone;
+    tdAddress.innerText = person.location.city + ' ' + person.location.state;
+    tdPicture.innerHTML = `<img src="${person.picture.thumbnail}" alt="${person.name.first} ${person.name.last}">`;
+
+    const tds = [tdName, tdPicture, tdAge, tdEmail, tdPhone, tdAddress];
+    if (viewportWidth < 768) {
+      headers.forEach((header, id) => {
+        const tr = document.createElement('tr');
+
+        const headerElement = document.createElement('th');
+        headerElement.innerText = header;
+
+        tr.append(headerElement, tds[id]);
+        table.appendChild(tr);
+      });
+      tr.className = 'divider';
+      table.appendChild(tr);
+    } else {
+      tr.append(tdName, tdPicture, tdAge, tdEmail, tdPhone, tdAddress);
+      table.appendChild(tr);
+    }
+  });
+}
+
+async function onStart() {
+  const viewportWidth = window.innerWidth;
+  const paragraph = document.querySelector('#bottom-paragraph');
+
+  changeBacgroundEveryFifthRefreash(paragraph);
+  // const button = document.querySelector('#button');
+  // const chart = document.querySelector('#chart');
+  const loader = document.querySelector('#loader');
+  const data = await fetchData();
+  hydrateTable(data, viewportWidth);
+  loader.style.display = 'none';
 }
 
 onStart();
